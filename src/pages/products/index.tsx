4b8c924fdbcf412d588d219c98a2ca00d8078ee1
Minto <xpeed96@gmail.com>
2023-03-18 11:01:20 +0900

    Static Routing (in pages)

---
+++ src/pages/products/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ProductsPage() {
+  return <div>제품 소개 페이지</div>;
+}
+
+export default ProductsPage;
---
+++ src/pages/products/pants.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PantsPage() {
+  return <div>바지 설명 페이지</div>;
+}
+
+export default PantsPage;
