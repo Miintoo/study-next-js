5d1466ba19fdff36251ddc9e6fac3b2b9b844a68
Minto <xpeed96@gmail.com>
2023-03-25 10:35:37 +0900

    2023-03-25

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Link from 'next/link';
 import { getProducts } from '@/service/products';
 
-export default function ProductsPage() {
-  const products = getProducts();
+export default async function ProductsPage() {
+  const products = await getProducts();
   return (
     <>
       <h1>제품 소개 페이지</h1>
       <ul>
-        {products.map((product, index) => {
+        {products.map(({ id, name }, index) => {
           return (
             <li key={index}>
-              <Link href={`/products/${product}`}>{product}</Link>
+              <Link href={`/products/${id}`}>{name}</Link>
             </li>
           );
         })}
